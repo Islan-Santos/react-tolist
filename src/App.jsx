fad4cc26a7f4450519026b24f31a1dcd1c7b91f1
Islan Santos <islan.santos212@gmail.com>
2024-01-10 10:14:45 -0300

    chore: listando os intens

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,49 @@ import { useState } from 'react'
 
 function App(){
 
-  return <div>Olá React!</div>;
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'Criar funcionalidade X no Sistema',
+      category: 'Trabalho',
+      isCompleted: false,
+    },
+    {
+      id: 1,
+      text: 'Criar funcionalidade X no Sistema',
+      category: 'Trabalho',
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: 'Ir para a academia',
+      category: 'Pessoal',
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: 'Estudar React',
+      category: 'Estudos',
+      isCompleted: false,
+    },
+  ])
+
+  return <div className="app">
+    <h1>Lista de Tarefas</h1>
+    <div className="todo-list">
+      {todos.map((todo)=>(
+        <div className="todo">
+          <div className="content">
+            <p>{todo.text}
+            <div className="category">
+            ({todo.category})
+            </div>
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>;
 }
 
 export default App;
